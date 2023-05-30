dc823d4b3a74ac206cc9b7f3416eef794c9ad974
Weekend <872285171@qq.com>
2023-05-30 17:21:03 +0800

    add search

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
@@ -7,12 +8,30 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "zh-CN",
-      title: "周末的博客",
+      title: "Gaming & Coding",
       description: "vuepress-theme-hope 的博客演示",
     },
   },
 
   theme,
+
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      // customFields: [
+      //   {
+      //     getter: (page) => page.frontmatter.category,
+      //     formatter: "分类：$content",
+      //   },
+      //   {
+      //     getter: (page) => page.frontmatter.tag,
+      //     formatter: "标签：$content",
+      //   },
+      // ],
+    }),
+  ],
 
   // Enable it with pwa
   // shouldPrefetch: false,
